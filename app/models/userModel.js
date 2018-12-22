e276e37a1e7bae5a8575e4e556dea1c4b65eb0e0
ASHLEY WARREN <ratin.kumar.2k@gmail.com>
2018-12-22 16:37:46 +0530

    Added dashboard

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -1,5 +1,23 @@
 const mongoose = require('mongoose');
+const Scheman = mongoose.Schema;
 
-// userschema
+const UserSchema = Scheman({
+"login":String,
+"avatar_url":String,
+"html_url":String,
+"gists_url":String,
+"organizations_url":String,
+"repos_url":String,
+"name":String,
+"bio":String,
+"public_repos":Number,
+"public_gists":Number,
+"followers":Number,
+"following":Number,
+"year" : String,
+"contributions":Number,
+"graph":String
+});
 
-const schema = mongoose.schema;
+
+module.exports = mongoose.model('posts',UserSchema)
--- app/routes/dashboardRouter.js
+++ app/routes/dashboardRouter.js
@@ -2,17 +2,32 @@ const router = require('express').Router();
 const key = require('../config/keys');
 const user = require('../models/userModel');
 const octokit = require('../util/octokit');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test',{ useNewUrlParser: true });
+mongoose.connection
+    .once('open',()=>console.log('CONNECTED!'))
+    .on('error',(err)=>{
+        console.log("COuld not connect as ",err)
+    });
 
-router.get('/', (req, res) => {
 
-  //Requests to server
-  octokit.repos.list().then(result => {
+router.get('/:username', (req, res) => {
+  var name = req.params.username;
+  //console.log(name)
+  user.find({"login":name}).exec(function(err,user){
+    console.log(user[0].year);
+    res.render('dashboard.ejs', {Name: user[0].login, Bio: user[0].bio, Year: user[0].year})
+  })
+  
 
-    // Storing data in database 
-    // Check if user already exists in database
-  }).catch((err) => {
-    console.log(err);
-  });
+  // //Requests to server
+  // octokit.repos.list().then(result => {
+
+  //   // Storing data in database 
+  //   // Check if user already exists in database
+  // }).catch((err) => {
+  //   console.log(err);
+  // });
 });
 
 module.exports = router;
