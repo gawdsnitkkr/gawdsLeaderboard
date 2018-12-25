a07439b02eb63e3981ae069a8a6d6039a88d6701
ShubhanshuGairola <gairolashubh@gmail.com>
2018-12-25 19:45:22 +0530

    new homepage and other pages updated to new theme

--- app/routes/dashboardRouter.js
+++ app/routes/dashboardRouter.js
@@ -19,8 +19,12 @@ async function graphGen(username,res){
       const  process = await spawn('python',["./graphGen.py", username]);
       process.stdout.on('data', function(data) { 
       console.log(data.toString())}); 
-      res.render('dashboard.ejs', {Name: user.login, Bio: user.bio, Year: user.year, Link : user.avatar_url});
-      
+      res.render('dashboard.ejs', {
+        Name: user.login, 
+        Bio: user.bio, 
+        Year: user.year, 
+        Link : user.avatar_url
+      });
       }
     })
   }
